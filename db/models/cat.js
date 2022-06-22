a00a89393e68efbbeceb08bdcaaefa0e5e026f5a
Roman Paramzin <filpoyma@gmail.com>
2022-06-22 12:43:54 +0300

    Update cat.js

--- db/models/cat.js
+++ db/models/cat.js
@@ -7,9 +7,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Dog, Dogscat }) {
+    static associate(models) {
       // define association here
-      this.belongsToMany(Dog, { through: Dogscat, foreignKey: 'cat_id' });
+      this.belongsToMany(models.Dog, { through: models.Dogscat, foreignKey: 'cat_id' });
     }
   }
   Cat.init(
